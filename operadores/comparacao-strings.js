8acf8432a425b5f1fa0f94d5dfe352ea574f119b
jv-obrabo <jv2404200717@gmail.com>
2024-05-09 10:19:13 -0400

    operadores/comparacao-strings.js

---
+++ operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringIguais ="Wanderson" =="Wanderson"; // true
+let stringDiferente = "wanderson" == "Wanderson";// false
+let stringMenor     = "A" < "B" ;    // true
+let stringMaior  = "C" > "D";      //false
+console.log(`Wanderson==Wanderson : ${stringIguais}`);
+console.log(`Wanderson==Wanderson : ${stringDiferentes}`);
+console.log(`A < B : ${stringMenor}`);
+console.log(`C > D  : ${stringMaior}`);
