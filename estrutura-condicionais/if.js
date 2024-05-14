f0cbca9872f8b8fe2119a081f7d10a496d3f3f34
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 10:13:46 -0400

    adicionado exemplo de estrutura-condicionais/

---
+++ estrutura-condicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12 ) {
+    console.log("Bom dia !") ;
+}
+ // o  restante do codigo continua independentemente se a condiçao for verdadeira ou nao 
+  console.log("Dinheiro nao traz felicidade. Me de o seu e seja feliz ")
