3c3b6e391e9dceaa0dcf2acd988c3df1a571a94e
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:37:02 -0400

    adicionado exemplo template-literals.js

---
+++ template-literals.js
@@ -0,0 +1,5 @@
+let nome = "Wanderson Timóteo";
+let idade = 30;
+// Usando template literals para criar uma string com variáveis
+let saudacao = Olá, meu nome é ${nome) e eu tenho ${idade) anos.`;
+console.log(saudacao); // Saida: "Olá, meu nome é Wanderson Timóteo e eu tenho 30 anos
