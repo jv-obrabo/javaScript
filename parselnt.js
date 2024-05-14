20310e31ef65cfcc495a73f1e3cc4e22c4858578
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:34:53 -0400

    adicionado exemplo de  parselnt.js

---
+++ parselnt.js
@@ -0,0 +1,7 @@
+let stringParaNumero = "100";
+ console.log(stringParaNumero); // Saida: "100"
+console.log(typeof(stringParaNumero)); // Saida: string 4 console.log(""); // Saida: "100"
+ //Conversão da String "stringParaNumero" para Number
+ let numero= parseInt(stringParaNumero);
+ console.log(numero); //Saida: 100
+ console.log(typeof(numero)); // Saida: number
