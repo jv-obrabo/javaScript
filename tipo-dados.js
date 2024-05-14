b2317d72e0a6a7da36113e46abf7549f453d2eaa
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:38:53 -0400

    adicionado exemplo de tipo-dados.js

---
+++ tipo-dados.js
@@ -0,0 +1,14 @@
+let nome = "Wanderson";
+let verdadeiro = true;
+let falso = false;
+let numero = 40;
+let reais 25.99;
+let nulο = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+    name: "Wanderson",
+    age: 40,   
+    Profession: "Teacher"
+
+}
