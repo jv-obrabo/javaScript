785eda874c77ac8c70b053a63b431943f2fd4cbb
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:23:20 -0400

    adicionado exemplo de concatenacao-string.js

---
+++ concatenacao-string.js
@@ -0,0 +1,5 @@
+et nome = "Ryan";
+
+idade = 11;
+
+et et saudacao = "Olá, meu nome é 11 + nome + e eu tenho + idade + " anos."; 11 11 console.log(saudacao); // Saida: "Olá, meu nome é Ryan e eu tenho 11 anos."
