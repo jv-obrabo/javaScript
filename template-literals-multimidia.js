fdac0a169e212df1c112575ddc6cfc8664ec28de
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:35:39 -0400

    adicionado exemplo template-literals-multimidia.js

---
+++ template-literals-multimidia.js
@@ -0,0 +1,6 @@
+let textoMultilinha = Esta é uma string que ocupa múltiplas linhas e você não precisa se preocupar com quebras de linha ou concatenação.
+console.log(texto Multilinha);
+/*
+Saida:
+Esta é uma string que ocupa múltiplas linhas e você não precisa se preocupar com quebras de linha ou concatenação.
+*/
