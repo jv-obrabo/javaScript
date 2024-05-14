3b62acf352017846ddd8a933fee6bd60ca08becc
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 10:15:16 -0400

    adicionado exemplo de estrutura-condicionais/if-else.js

---
+++ estrutura-condicionais/if-else.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if ( diaDaSemana==="Sabado" || diaDaSemana === "domingo") {
+    console.log("È fim de semana!") ;
+} else {
+    console.log("e dia de semana.")
+}
