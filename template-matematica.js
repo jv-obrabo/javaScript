48681f38ae9d28abfa636a785bdd9b82149a235e
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:37:50 -0400

    adicionado exemplo de  template-matematica.js

---
+++ template-matematica.js
@@ -0,0 +1,7 @@
+let preco 19.99;
+
+let desconto = 0.1;
+
+11
+
+✓ Usando template literals com expressões matemáticas et precoFinal = O preço final do produto com desconto é ${preco (1 desconto)} reais.'; onsole.log(precoFinal); // Saida: "O preço final do produto com desconto é 17.991 reais."
