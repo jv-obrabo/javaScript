779349142c34b546695fa63122de5df4fd18c83a
jv-obrabo <jv2404200717@gmail.com>
2024-05-14 08:34:10 -0400

    adicionado exemplo de  parseFloat.js

---
+++ parseFloat.js
@@ -0,0 +1,6 @@
+let stringParaDecimal = "12.60";
+ console.log(stringParaDecimal); // Saida: "12.60" console.log(typeof(stringParaDecimal)); // Saida: string 4 console.log(""); // Saida: "12.60"
+//Conversão da String "stringParaDecimal" para Decimal
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal); //Saida: 12.60
+console.log(typeof(decimal)); // Saída: number
